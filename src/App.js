979e6aa95d7a436ef746ebcd7ed564d1590f84cb
Ford Combs <fcombs90@gmail.com>
2023-08-24 09:52:11 -0400

    minor formatting fixes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const [textInput, setTextInput] = useState('');
-  const [responseText, setResponseText] = useState('');
+  const [textInput, setTextInput] = useState('enter some text');
+  const [responseText, setResponseText] = useState('see the results');
 
   const handleInputChange = (event) => {
     setTextInput(event.target.value);
@@ -22,7 +22,7 @@ function App() {
       const response = await fetch(URL, {
         method: 'POST',
         headers: {
-          'Authorization': 'Bearer ya29.a0AfB_byDMRTA6yzk4YU7oC6eVnU4uVosZbI-gTlhOygG6lAqFOgkSwLXOPsPkiIIhRcM9VPTWNdbYUbSKUZQvSA2deGjzSYkSF4ZpiAbq6_htKNuljni4K4qkrHQt_BXNYTHoMkRMVrKiM9Crvggho4XIpPWUfpO072hiqhPE3EtYOBcaCgYKARMSARASFQHsvYlswzEEf0YyB9tcHHJwwytDyg0182',
+          'Authorization': 'Bearer ya29.a0AfB_byCZFBfYhea9Zo9n8nWDT-j4wrmxha2XigKuRvOzXzL3zSIU4QnU7pNQcB_eGNmsl0MaU_su8nxyoCig22UUYHbdCgc7trqVz8mVV_oEZvWilR7fR6dnS1DMSNck95gdX-meDt7e4TOQI-PlBoni0ymWes7KHkAmCmwh-eH72eIaCgYKASESARASFQHsvYls5Wp2wyaCPDAXvkxgaFDT8A0182',
           'Content-Type': 'application/json; charset=utf-8'
         },
         body: JSON.stringify(predjson),
@@ -30,19 +30,27 @@ function App() {
 
       if (response.ok) {
         const responseData = await response.json()
+        console.log(responseData)
         const a = responseData.predictions[0].confidences[0];
         const b = responseData.predictions[0].confidences[1];
         let responseOutput = '';
+        let responseMessage = '';
         if (a > b) {
-          console.log("a is greater than b");
+          if (a > 0.85){
+            responseMessage = "This is very biased to the "
+          } else {
+            responseMessage = "This is biased to the "
+          }
           responseOutput = responseData.predictions[0].displayNames[0];
         } else {
-          console.log("b is greater than a");
+          if (b > 0.85){
+            responseMessage = "This is very biased to the "
+          } else {
+            responseMessage = "This is biased to the "
+          }
           responseOutput = responseData.predictions[0].displayNames[1];
         }
-        // console.log(responseOutput);
-        setResponseText(responseOutput);
-        // setResponseText(JSON.stringify(responseData));
+        setResponseText(responseMessage+responseOutput);
       } else {
         console.error('Error sending POST request');
       }
@@ -58,18 +66,14 @@ function App() {
     {/* <div className="App"> */}
     <div className='container'>
       {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      <div className='textbox-container'>
       <form onSubmit={handleSubmit}>
-        <label>
-          {/* Enter text: */}
-          <textarea value={textInput} rows={3} onChange={handleInputChange}></textarea>
-          <textarea value={responseText} rows={3} readOnly></textarea>
-          {/* <input type="text" value={textInput} onChange={handleInputChange} /> */}
-        </label>
+      <div className='textbox-container'>
+            <textarea value={textInput} rows={3} onChange={handleInputChange}></textarea>
+            <textarea value={responseText} rows={3} readOnly></textarea>
+        </div>
         <button type="submit">Submit</button>
       </form>
-      {responseText && <p>Response: {responseText}</p>}
-    </div>
+
     </div>
     </>
   );
